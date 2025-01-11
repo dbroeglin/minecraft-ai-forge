809af00f378b11c2505e2c6d9fe91e7ab5125ca2
Dominique Broeglin <dominique.broeglin@gmail.com>
2025-01-11 14:19:09 +0100

    Upgrading to JDK 23 and Langchain4J 1.0.0-alpha1

--- src/main/java/fr/broeglin/mcai/AiCommand.java
+++ src/main/java/fr/broeglin/mcai/AiCommand.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.logging.LogUtils;
 import dev.langchain4j.model.azure.AzureOpenAiChatModel;
 import net.minecraft.ChatFormatting;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
@@ -27,20 +28,22 @@ public class AiCommand {
                             String endpoint = System.getenv("AZURE_OPENAI_ENDPOINT");
                             String deploymentName = System.getenv("AZURE_OPENAI_DEPLOYMENT_NAME");
 
-                            AzureOpenAiChatModel model = AzureOpenAiChatModel.builder()
+                            ChatLanguageModel model = AzureOpenAiChatModel.builder()
                                     .apiKey(key)
-                                    .endpoint(endpoint)
                                     .deploymentName(deploymentName)
+                                    .endpoint(endpoint)
                                     .temperature(0.3)
                                     .logRequestsAndResponses(true)
                                     .build();
-                            String response = model.generate("Provide a short answer (max 250 characters) to the following question: " + question);
+                            String response = model
+                                    .generate("Provide a short answer (max 250 characters) to the following question: "
+                                            + question);
 
                             context.getSource().sendSuccess(
                                     () -> Component.literal("AI: " + response)
-                                            .withStyle(ChatFormatting.ITALIC), true);
+                                            .withStyle(ChatFormatting.ITALIC),
+                                    true);
                             return 1; // Command was executed successfully
-                        }))
-        );
+                        })));
     }
 }
